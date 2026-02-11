9981fee57740d465f0d0be6a49917ce9a5da6a79
Bhushan-Nhavi <139908843+Bhushan-Nhavi@users.noreply.github.com>
2026-02-11 21:09:14 +0530

    Add files via upload

---
+++ Assistance/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from Assistance.models import *
+
+# Register your models here.
+
+admin.site.register(Regist)
+admin.site.register(Profile)
+admin.site.register(Mechanic_Regist)
+admin.site.register(Mechanic_Profile)
+admin.site.register(Order)
---
+++ Assistance/apps.py
@@ -0,0 +1,14 @@
+from django.apps import AppConfig
+
+
+class AssistanceConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Assistance'
+
+
+
+# Project----Vehicle
+#Apps--------Assistance
+#
+#
+#
